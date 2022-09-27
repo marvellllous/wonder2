a5d113155f85c58104e0d001804e3972008ece8f
690178 <690178@A21591>
2022-09-27 15:01:11 +0100

    Add project files.

---
+++ wonder/wonder/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace wonder
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+       
+
+       
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StudentProfile());
+        }
+
+        private void AdminLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
